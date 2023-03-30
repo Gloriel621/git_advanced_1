747eccf72557f43a00162b0653defee47b54c159
Hakjun Moon <gloriel621@gmail.com>
2023-03-30 19:35:45 +0900

    update even_list

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ from typing import List
 
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
-    """
-    Determines if a number is even and return an even list.
+    
+    even_list = []
 
-    Args:
-        int_list: A list of integer.
-
-    Returns:
-        A list of even integers.
-    """
-    # TODO: Implement even_list
-    pass
+    for integer in int_list:
+        if integer % 2 == 0:
+            even_list.append(integer)
+            
+    return even_list
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
@@ -20,7 +17,6 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     Computes the sum of the squares of all even numbers in a list of integers.
     Args:
     even_int_list: A list of even integers.
-
     Returns:
     The sum of the squares of all even numbers in the list.
     """
